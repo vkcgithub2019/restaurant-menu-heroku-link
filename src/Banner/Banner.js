159be8a5dfc6e104d13ccdb47395de9883862fc2
Victor Chung <victochung998@yahoo.com>
2019-12-02 18:15:32 -0800

    shrinked banner height

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Banner = styled.div`
-   height: 200px;
+  /*  height: 200px; */
+   height: 15px;
    /* background-image: url("img/map-of-Malaysia.jpg"); */
    background-position: center;
    background-size: cover;
